e89f11c4861841c08feeabc4bb83da1acae65288
Alex Dunne <hi@alexdunne.net>
2018-03-17 19:25:13 +0000

    Added a site (built with gatsby) to host a privacy policy for the app

---
+++ site/src/layouts/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+import './index.css'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet title="React native animations exampls" />
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper

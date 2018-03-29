f2b70948ff7c61d3aedab811e0f7445032fa8d83
Alex Dunne <hi@alexdunne.net>
2018-03-30 00:47:42 +0100

    [TicketPrint]  Added the ticket print box

--- src/examples/TicketPrint/App.js
+++ src/examples/TicketPrint/App.js
@@ -1,5 +1,31 @@
 import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 32,
+    paddingHorizontal: 32,
+    backgroundColor: "#00B9F1"
+  },
+  tickerPrinter: {
+    alignSelf: "stretch",
+    height: 24,
+    backgroundColor: "#000000",
+    borderWidth: 8,
+    borderColor: "#312931",
+    borderRadius: 16
+  }
+});
 
 class TicketPrint extends Component {
-  render() {}
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.tickerPrinter} />
+      </View>
+    );
+  }
 }
+
+export default TicketPrint;

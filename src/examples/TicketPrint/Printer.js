f64f5cd99e76416d5d32fcd8c5cb0ca683b2cb13
Alex Dunne <hi@alexdunne.net>
2018-03-30 01:19:40 +0100

    [TicketPrint] Renderd the Printer children outside of the printer box and used marginTop to move the top of the child into the centre of the printer area

--- src/examples/TicketPrint/Printer.js
+++ src/examples/TicketPrint/Printer.js
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
-  tickerPrinter: {
+  container: {
+    flex: 1
+  },
+  printer: {
     alignSelf: "stretch",
     height: 24,
     backgroundColor: "#000000",
     borderWidth: 8,
     borderColor: "#312931",
     borderRadius: 16
+  },
+  printerOffset: {
+    marginTop: -12,
+    paddingHorizontal: 16
   }
 });
 
 const Printer = ({ children }) => (
-  <View style={styles.tickerPrinter}>{children}</View>
+  <View style={styles.container}>
+    <View style={styles.printer} />
+    <View style={styles.printerOffset}>{children}</View>
+  </View>
 );
 
 export default Printer;
